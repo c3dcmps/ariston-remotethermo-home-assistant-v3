b9ff1c4300af9b04ad90112dfd2eadb2e54d1aef
c3dcmps <github.ht6mv@slmail.me>
2026-05-30 11:42:46 +0200

    Add BSB_PRESET_COMFORT and BSB_PRESET_REDUCED constants

--- custom_components/ariston/const.py
+++ custom_components/ariston/const.py
@@ -60,6 +60,8 @@ API_URL_SETTING: Final[str] = "api_url_setting"
 API_USER_AGENT: Final[str] = "api_user_agent"
 CONF_BRAND: Final[str] = "brand"
 ELCO_API_URL: Final[str] = "https://www.remocon-net.remotethermo.com"
+BSB_PRESET_COMFORT: Final[str] = "Confort"
+BSB_PRESET_REDUCED: Final[str] = "Réduit"
 
 DEFAULT_SCAN_INTERVAL_SECONDS: Final[int] = 180
 DEFAULT_ENERGY_SCAN_INTERVAL_MINUTES: Final[int] = 60
